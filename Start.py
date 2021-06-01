d2729e09c367b79bd08089dada64d044ae782993
nehalsekhon <83484354+nehalsekhon@users.noreply.github.com>
2021-05-31 19:35:29 -0600

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,44 @@
+# -------------------------Import Libraries------------------------------#
+import numpy as np
+import open3d as o3
+import trimesh
+import proberg
+
+clearSet = {"r*", "data*", "plot*", "file*", 'datafile', 'datasize', 'data3D', \
+  'STDcol', 'numebrofthre', 'directory', 'fnames', 'Numberofolders', 'csvName', \
+  'csvLength', 'paths'}
+clearSet.clear
+
+# 2. Be able to open data set
+Threshold1 = {3, 9.333}
+initial = 1
+initial1 = 1
+numebrofthre = len(Threshold1)
+
+#directory to the EDM file 
+path = 'C:\Users\nehal\OneDrive\Desktop\Research ST'
+file = np.loadtxt(path +"EDM-1.csv")
+
+
+# #open csv files
+# open('EDM-1.csv') as csvfile:
+
+
+# directory = SystemDialogInput["Directory"]
+
+# fn1 = FileNames[] numberoffiles=Length[fn1]
+# if directory == Canceled, SetDirectory(directory)
+
+# fnames = FileNames("EDM-1.csv") #finds the relative path to each file
+# Numberofolders = fnames // Length; #total number of folders
+# csvName = "EDM-1.csv" #sets the default name of the csv
+# csvLength = StringLength[csvName] #length of the csv file
+# pathLength = StringLength[fnames[[#]]] & /@ 
+#   Range@Numberofolders; (*length of the total path for\ each folder*)
+
+# paths = StringTake[fnames[[#]], pathLength[[#]] - csvLength] & /@ 
+#   Range@Numberofolders;(*directory\ of each folder*)
+# Print["Folder order to check=" <> ToString@paths];
+# ProgressIndicator[Dynamic[countfile/Numberofolders]]
+#Import Libraries
+#import numpy as np
