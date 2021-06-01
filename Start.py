878464d99ec02b605f223f3ca8445d0d52f5569c
Clarence Cheung <83518886+clarencehycheung@users.noreply.github.com>
2021-06-01 11:56:21 -0600

    Update Start.py

--- Start.py
+++ Start.py
@@ -15,16 +15,6 @@ initial = 1
 initial1 = 1
 numebrofthre = len(Threshold1)
 
-#directory to the EDM file 
+# directory to the EDM file 
 path = 'C:/Users/nehal/OneDrive/Desktop/Research ST/'
 file = np.loadtxt(path + 'EDM-1.csv')
-
-# directory = SystemDialogInput["Directory"]
-# fn1 = FileNames[] numberoffiles=Length[fn1]
-# if directory == Canceled, SetDirectory(directory)
-
-
-# paths = StringTake[fnames[[#]], pathLength[[#]] - csvLength] & /@ 
-#   Range@Numberofolders;(*directory\ of each folder*)
-# Print["Folder order to check=" <> ToString@paths];
-# ProgressIndicator[Dynamic[countfile/Numberofolders]]
