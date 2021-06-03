5aaea380edd51b601b91d0a930736eb086c86255
nehalsekhon <83484354+nehalsekhon@users.noreply.github.com>
2021-06-02 18:06:50 -0600

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -9,7 +9,11 @@ path = askdirectory(title='Select Folder')
 
 # Loops through the subfolders in the parent folder and finds the EDM-1.csv files
 for subdir, dirs, files in os.walk(path):
-  for file in files:
-    filepath = subdir + os.sep + file
-    if filepath.endswith("EDM-1.csv"):
-      print(filepath)
+    for file in files:
+        filepath = subdir + os.sep + file
+        if filepath.endswith("EDM-1.csv"):
+            datafile = pd.read_csv(filepath, header=None)
+            # Delete columns
+            datafile.drop([0, 1, 2, 6, 7, 8], axis=1, inplace=True)
+            # Rename columns
+            data3D = datafile.rename(columns={3: 0, 4: 1, 5: 2, 9: 3}, inplace=False)
