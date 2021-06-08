e4d38b3414163b24e301eab6c6f3e03bc1fc4a35
Clarence Cheung <83518886+clarencehycheung@users.noreply.github.com>
2021-06-08 15:48:19 -0600

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from tkinter.filedialog import askdirectory
 import os
+from sympy import Point3D, Plane
 
 # Prompting user to select the parent folder
 path = askdirectory(title='Select Folder')
@@ -17,3 +18,9 @@ for subdir, dirs, files in os.walk(path):
             datafile.drop([0, 1, 2, 6, 7, 8], axis=1, inplace=True)
             # Rename columns
             data3D = datafile.rename(columns={3: 0, 4: 1, 5: 2, 9: 3}, inplace=False)
+
+# Finding height of torso
+maxy = max(data3D)
+miny = min(data3D)
+xzplane01 = Plane(Point3D(0, maxy, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
+xzplane02 = Plane(Point3D(0, miny, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
