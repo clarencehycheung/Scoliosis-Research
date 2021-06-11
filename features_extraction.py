560130af8cdc9deeb5f7892904e30c8af19c948d
nehalsekhon <83484354+nehalsekhon@users.noreply.github.com>
2021-06-11 16:19:12 -0600

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -27,12 +27,18 @@ for subdir, dirs, files in os.walk(path):
             # Test coordinates
             data3D.drop([4], axis=1, inplace=True)
             # Rotate points for Brazil data
+            data3D = data3D.reindex(columns=[1, 3, 2])
+            data3D = data3D.rename(columns={1: 1, 3: 2, 2: 3}, inplace=False)
+            data3D[1] = -1*data3D[1]
 
 
             # Finding height of torso
-            maxy = max(data3D[2])
-            maxy3D = Point3D(0, maxy, 0)
-            miny = min(data3D[2])
+            maxy = data3D.iloc[data3D[2].idxmax()]
+            maxy = [maxy.iloc[0], maxy.iloc[1], maxy.iloc[2]]
+            miny = data3D.iloc[data3D[2].idxmin()]
+            miny = [miny.iloc[0], miny.iloc[1], miny.iloc[2]]
+            
+            
             xzplane01 = Plane(Point3D(0, maxy, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             xzplane02 = Plane(Point3D(0, miny, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             # Find distance between xzplane02 and maxy
@@ -41,18 +47,24 @@ for subdir, dirs, files in os.walk(path):
 
 
             # Finding width of torso
-            maxx = max(data3D[1])
-            maxx3D = Point3D(0, maxx, 0)
-            minx = min(data3D[1])
+            maxx = data3D.iloc[data3D[1].idxmax()]
+            maxx = [maxx.iloc[0], maxx.iloc[1], maxx.iloc[2]]
+            minx = data3D.iloc[data3D[1].idxmin()]
+            minx = [minx.iloc[0], minx.iloc[1], minx.iloc[2]]
+    
+            
             yzplane01 = Plane(Point3D(0, maxx, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             yzplane02 = Plane(Point3D(0, minx, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             # Find distance between yzplane02 and maxx
             twidth = maxx3D.distance(yzplane02)
 
             # Finding depth of torso
-            maxz = max(data3D[3])
-            maxz3D = Point3D(0, maxz, 0)
-            minz = min(data3D[3])
+            maxz = data3D.iloc[data3D[3].idxmax()]
+            maxz = [maxz.iloc[0], maxz.iloc[1], maxz.iloc[2]]
+            minz = data3D.iloc[data3D[3].idxmin()]
+            minz = [minz.iloc[0], minz.iloc[1], minz.iloc[2]]
+            
+            
             xyplane01 = Plane(Point3D(0, maxz, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             xyplane02 = Plane(Point3D(0, minz, 0), Point3D(1, 0, 0), Point3D(0, 0, 1))
             # Find distance between xyplane02 and maxz
